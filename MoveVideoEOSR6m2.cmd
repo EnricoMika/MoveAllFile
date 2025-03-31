69c824897fee4d3098a7dc162ad14b8f21e70736
EnricoMika <74066180+EnricoMika@users.noreply.github.com>
2025-03-31 17:05:24 +0200

    MoveVideoEOSR6m2.cmd

--- MoveVideoEOSR6m2.cmd
+++ MoveVideoEOSR6m2.cmd
@@ -3,7 +3,7 @@ setlocal enabledelayedexpansion
 
 set source_folder=D:\Immages\Camera                         rem paste here the path of the source folder (important to use \)
 set destination_folder=D:\Videos\Camera                     rem paste here the path of the destination folder (important to use \)
-set file_extension=.mp4                                     rem set here the type of the file you want to move
+set file_extension=.mp4                                     rem set here the type of the file you want to move (use the point first)
 
 for /r "%source_folder%" %%i in (*%file_extension%) do (
     set "file=%%~nxi"
