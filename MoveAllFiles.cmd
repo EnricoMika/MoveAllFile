bcbd46398d3167a4d2e55edadf35f60d0be3f634
EnricoMika <74066180+EnricoMika@users.noreply.github.com>
2025-03-31 17:45:25 +0200

    MoveAllFiles.cmd

--- MoveAllFiles.cmd
+++ MoveAllFiles.cmd
@@ -1,9 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set source_folder=D:\Immages\Camera                         rem paste here the path of the source folder (important to use \)
-set destination_folder=D:\Videos\Camera                     rem paste here the path of the destination folder (important to use \)
-set file_extension=.mp4                                     rem set here the type of the file you want to move (use the point first)
+set source_folder=D:\Immages\Camera                         /paste here the path of the source folder (important to use \)/
+set destination_folder=D:\Videos\Camera                     /paste here the path of the destination folder (important to use \)/
+set file_extension=.mp4                                     /set here the type of the file you want to move (use the point first)/
 
 for /r "%source_folder%" %%i in (*%file_extension%) do (
     set "file=%%~nxi"
